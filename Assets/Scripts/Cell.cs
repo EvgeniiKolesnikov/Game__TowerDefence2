f78aa784ed39c2ba5ded73fc9c18cef73edc1c75
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-13 21:31:28 +0300

    Day 3. Add new initial files and Tower prefab

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    public Material mainMaterial;
-    public Material overMaterial;
-    public bool canBuild;
+  public Material mainMaterial;
+  public Material canMaterial;
+	public Material cantMaterial;
+  public bool canBuild;
 
-	private void OnMouseOver()
-	{
-		if (canBuild)
-		{
-			GetComponent<Renderer>().material = overMaterial;
+  private void OnMouseOver()
+  {
+    if (canBuild)
+    {
+      GetComponent<Renderer>().material = canMaterial;
+    } else {
+			GetComponent<Renderer>().material = cantMaterial;
 		}
-	}
+  }
 
-	private void OnMouseExit()
-	{
-		GetComponent<Renderer>().material = mainMaterial;
-	}
+  private void OnMouseExit()
+  {
+    GetComponent<Renderer>().material = mainMaterial;
+  }
 }
